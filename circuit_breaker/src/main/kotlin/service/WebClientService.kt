016111b6f3ae0a8e79f345eb93c1cf0c88fa2cf4
Jhzlo <kjh0010703@naver.com>
2025-03-01 15:44:50 +0900

    feat: error catch

--- circuit_breaker/src/main/kotlin/service/WebClientService.kt
+++ circuit_breaker/src/main/kotlin/service/WebClientService.kt
@@ -3,7 +3,9 @@ package com.jhzlo.service
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
+import java.util.concurrent.atomic.AtomicInteger
 
 @Service
 class WebClientService(
@@ -12,6 +14,10 @@ class WebClientService(
     companion object {
         const val API = "/api/random-error"
     }
+    private val successCount = AtomicInteger(0)
+    private val badRequestCount = AtomicInteger(0)
+    private val internalServerErrorCount = AtomicInteger(0)
+    private val circuitBreakerBlockedCount = AtomicInteger(0)
 
     @CircuitBreaker(name = "default", fallbackMethod = "fallbackResponse")
     fun fetchData(): Mono<String> {
@@ -19,7 +25,29 @@ class WebClientService(
         return webClient.get()
             .uri("/api/random-error")
             .retrieve()
+            .onStatus({ status -> status.is4xxClientError }) { response ->
+                // 400 에러
+                badRequestCount.incrementAndGet()
+                println("400 ERROR: ${response.statusCode()}")
+                Mono.empty()
+            }
+            .onStatus({ status -> status.is5xxServerError }) { response ->
+                // 500 에러
+                internalServerErrorCount.incrementAndGet()
+                println("500 ERROR: ${response.statusCode()}")
+                Mono.empty()
+            }
             .bodyToMono(String::class.java)
+            .doOnSuccess {
+                // 요청 성공
+                successCount.incrementAndGet()
+            }
+            .doOnError { error ->
+                // CircuitBreaker로 인해 실패
+                if (error is io.github.resilience4j.circuitbreaker.CallNotPermittedException) {
+                    circuitBreakerBlockedCount.incrementAndGet()
+                }
+            }
     }
 
     fun fallbackResponse(ex: Throwable): Mono<String> {
