6be04d621b90682f749e93cdd7aee796e2767040
Jhzlo <010703kjh@gmail.com>
2025-03-02 22:25:35 +0900

    Revert "Circuit Breaker 적용"

--- circuit_breaker/src/main/kotlin/dto/ResponseStats.kt
+++
@@ -1,8 +0,0 @@
-package com.jhzlo.dto
-
-data class ResponseStats(
-    val successCount: Int,
-    val badRequestCount: Int,
-    val internalServerErrorCount: Int,
-    val circuitBreakerBlockedCount: Int
-)
